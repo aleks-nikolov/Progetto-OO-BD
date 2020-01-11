4f99543a7cf96a5700543f33d8b65ad2983ade24
aleks-nikolov <a.nikolov@studenti.unina.it>
2020-01-11 17:48:09 +0100

    creazione pacchetto gui

---
+++ Sistema Gestionale/src/gui/HomePage.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class HomePage extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public HomePage() {
+		setTitle("Home Page");
+		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
